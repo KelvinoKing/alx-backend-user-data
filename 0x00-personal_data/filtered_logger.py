fac0293186db1da3f823796b3cf105027a999cef
KelvinoKing <kelvinoking@outlook.com>
2024-04-13 14:21:36 +0300

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -55,3 +55,31 @@ class RedactingFormatter(logging.Formatter):
         """ Format method"""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+"""
+Use user_data.csv for this task
+Implement a get_logger function that takes no arguments and returns a
+logging.Logger object.
+The logger should be named "user_data" and only log up to logging.INFO level.
+It should not propagate messages to other loggers. It should have a StreamHandler
+with RedactingFormatter as formatter.
+Create a tuple PII_FIELDS constant at the root of the module containing
+the fields from user_data.csv that are considered PII. PII_FIELDS can contain
+only 5 fields - choose the right list of fields that can are considered as “important”
+PIIs or information that you must hide in your logs.
+Use it to parameterize the formatter."""
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def get_logger() -> logging.Logger:
+    """ Returns a logging.Logger object"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
