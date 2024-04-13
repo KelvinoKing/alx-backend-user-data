773dabd835e744dded4361eb9aacac718a1336d5
KelvinoKing <kelvinoking@outlook.com>
2024-04-13 15:29:58 +0300

    Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -21,6 +21,9 @@ import mysql.connector
 from mysql.connector import connection
 
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Returns the log message obfuscated"""
@@ -75,9 +78,6 @@ considered as “important” PIIs or information that you must hide in your log
 Use it to parameterize the formatter."""
 
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
-
-
 def get_logger() -> logging.Logger:
     """ Returns a logging.Logger object"""
     logger = logging.getLogger('user_data')
@@ -106,3 +106,48 @@ def get_db() -> connection.MySQLConnection:
         host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
         database=os.getenv('PERSONAL_DATA_DB_NAME', '')
     )
+
+
+"""
+Implement a main function that takes no arguments and returns nothing.
+The function will obtain a database connection using get_db and retrieve
+all rows in the users table and display each row under a filtered
+format like this
+[HOLBERTON] user_data INFO 2019-11-19 18:37:59,596: name=***; email=***;
+phone=***; ssn=***; password=***; ip=e848:e856:4e0b:a056:54ad:1e98:8110:ce1b;
+last_login=2019-11-14T06:16:24; user_agent=Mozilla/5.0 (compatible; MSIE 9.0;
+Windows NT 6.1; WOW64; Trident/5.0; KTXN);
+Filtered fields:
+name
+email
+phone
+ssn
+password
+Only your main function should run when the module is executed.
+"""
+
+
+def main():
+    """Logs the information about user records in a table.
+    """
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
+
+
+if __name__ == '__main__':
+    main()
