cea105e2a5f0a92d4152735a8802f2e49cf00b9f
KelvinoKing <kelvinoking@outlook.com>
2024-04-16 13:52:34 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Create the class BasicAuth that inherits from Auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,3 +21,23 @@ class BasicAuth(Auth):
         if authorization_header[:6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """Decode base64 authorization header method
+        Otherwise, return the decoded value as UTF8 string
+        - you can use decode('utf-8')
+        """
+        if base64_authorization_header is None\
+                or type(base64_authorization_header) is not str:
+            return None
+        try:
+            decode_bytes = base64.b64decode(
+                base64_authorization_header
+            )
+            decoded_str = decode_bytes.decode('utf-8')
+            return decoded_str
+        except Exception as e:
+            return None
