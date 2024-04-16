784c6de46e77ce60b5e82975c8767e3ee112d72d
KelvinoKing <kelvinoking@outlook.com>
2024-04-16 14:05:00 +0300

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,3 +41,31 @@ class BasicAuth(Auth):
             return decoded_str
         except Exception as e:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """
+        Add the method def extract_user_credentials(self,
+        decoded_base64_authorization_header: str) -> (str, str)
+        in the class BasicAuth that returns the user email and
+        password from the Base64 decoded value.
+        This method must return 2 values
+        Return None, None if decoded_base64_authorization_header is None
+        Return None, None if decoded_base64_authorization_header
+        is not a string
+        Return None, None if decoded_base64_authorization_header doesn’t
+        contain :
+        Otherwise, return the user email and the user password -
+        these 2 values must be separated by a :
+        You can assume decoded_base64_authorization_header will
+        contain only one :
+        """
+        if decoded_base64_authorization_header is None\
+                or type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        user_credentials = decoded_base64_authorization_header.split(':')
+        return user_credentials[0], user_credentials[1]
