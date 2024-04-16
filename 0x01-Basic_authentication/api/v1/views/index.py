d5cda9e482c9bc9666a83835518301ca8f2ddbbf
KelvinoKing <kelvinoking@outlook.com>
2024-04-16 12:22:17 +0300

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -19,6 +19,15 @@ def unauthorized() -> str:
     abort(401)
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Raise:
+      - 403 error
+    """
+    abort(403)
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
