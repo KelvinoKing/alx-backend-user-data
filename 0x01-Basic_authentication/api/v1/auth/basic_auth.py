0853e8b511aec90c26d1d4c6eb4425e56870c17e
KelvinoKing <kelvinoking@outlook.com>
2024-04-16 13:19:45 +0300

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Create the class BasicAuth that inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class
+    """
+    pass
