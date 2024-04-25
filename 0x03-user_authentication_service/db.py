e7da97dd33531d9977e983811ea8e80942f20765
KelvinoKing <kelvinoking@outlook.com>
2024-04-25 11:35:17 +0300

    Log in

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,10 +40,11 @@ class DB:
             user = User(email=email, hashed_password=hashed_password)
             self._session.add(user)
             self._session.commit()
+            return user
         except Exception:
             self._session.rollback()
             user = None
-        return user
+            return user
 
     def find_user_by(self, **kwargs) -> User:
         """Find a user by a given attribute
