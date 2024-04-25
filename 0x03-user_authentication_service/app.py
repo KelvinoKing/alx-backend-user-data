9c53018aa464d4df18163e87eeed0a53464e1ac4
KelvinoKing <kelvinoking@outlook.com>
2024-04-25 11:01:00 +0300

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,9 +3,10 @@
 Create a Flask app that has a single GET route ("/")
 and use flask.jsonify to return a JSON payload of the form:
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from auth import Auth
 from flask import request
+from sqlalchemy.orm.exc import NoResultFound
 
 Auth = Auth()
 app = Flask(__name__)
@@ -50,6 +51,29 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    In this task, you will implement a login function to
+    respond to the POST /sessions route.
+    The request is expected to contain form data with "email"
+    and a "password" fields.
+    If the login information is incorrect, use flask.abort
+    to respond with a 401 HTTP status.
+    Otherwise, create a new session for the user, store it the
+    session ID as a cookie with key "session_id" on the response
+    and return a JSON payload of the form
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if Auth.valid_login(email, password):
+        session_id = Auth.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    abort(401)
 
 
 if __name__ == "__main__":
