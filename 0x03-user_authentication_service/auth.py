404cf1097ccc71ddaec54ea06a89733a8abc0e36
KelvinoKing <kelvinoking@outlook.com>
2024-04-25 13:45:38 +0300

    Generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,8 +91,7 @@ class Auth:
             return None
         try:
             user = self._db.find_user_by(id=user_id)
-            user.session_id = None
-            self._db._session.commit()
+            self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return None
         return None
